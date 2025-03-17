cafe53114137766eb68a695d83d63e0f67cea017
Tom <tomzarb98@gmail.com>
2025-03-17 01:13:58 +0100

    [MEAL-8] Add Welcome and Profile pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
-const root = document.getElementById("root");
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
+import Welcome from "./pages/Welcome.jsx"; 
+import Profile from "./pages/profile.jsx"; 
+
+const root = document.getElementById("root");
 
 createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Welcome />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
